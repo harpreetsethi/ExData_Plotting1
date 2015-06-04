2a884f503b4d1b08eff3eb9211657337dc6e30cb
harpreetsethi <harpreet.singh.sethi@gmail.com>
2015-06-04 16:37:36 +0100

    Version 2
    auto computes the location of the second day and new file for plot 3

--- plot3.R
+++ plot3.R
@@ -34,13 +34,20 @@ var_dataTbl<-tbl_df(read.csv.sql(paste(cwd,"/household_power_consumption.txt", s
                                  sep=";", header=TRUE))
 var_dataTbl<-tbl_df(mutate(var_dataTbl, dayofweek=wday(var_dataTbl$Date, label=TRUE)))
 
-png(filename=paste(cwd,"/plot2.png", sep = ""), width = 480, height = 480, units = "px", bg = "white")
-plot((1:nrow(var_dataTbl)), var_dataTbl$Global_active_power, pch=NA_integer_,axes = FALSE,
-     ylab="Global Active Power (kilowatts)", xlab="")
-lines(x=(1:nrow(var_dataTbl)), y=var_dataTbl$Global_active_power)
+png(filename=paste(cwd,"/plot3.png", sep = ""), width = 480, height = 480, units = "px", bg = "white")
+minY<-min(var_dataTbl$Sub_metering_1, var_dataTbl$Sub_metering_2, var_dataTbl$Sub_metering_3)
+maxY<-max(var_dataTbl$Sub_metering_1, var_dataTbl$Sub_metering_2, var_dataTbl$Sub_metering_3)
+
+plot((1:nrow(var_dataTbl)), var_dataTbl$Sub_metering_1, xlim=c(1,nrow(var_dataTbl)),
+  ylim=c(minY,maxY+10), pch=NA_integer_,axes = FALSE,ylab="Energy Sub metering", xlab="")
+
+lines(x=(1:nrow(var_dataTbl)), y=var_dataTbl$Sub_metering_1)
+lines(x=(1:nrow(var_dataTbl)), y=var_dataTbl$Sub_metering_2, col="red")
+lines(x=(1:nrow(var_dataTbl)), y=var_dataTbl$Sub_metering_3, col="blue")
 axis(1, 1, c("Sat"))
 axis(1, head(which(grepl("Sun", var_dataTbl$dayofweek)),1), c("Sun"))
-axis(2, at=pretty(var_dataTbl$Global_active_power))
+axis(2, at=c(0,5,10,15,20,25,30,35))
 box()
-
+legend("topright", pch="-", col=c("black", "red", "blue"), 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
